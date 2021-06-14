5c3c61e879583d8617d3dd921eb9e530175ea6f4
Tenzin Choedak <tenzin.choedak@powercoders.com>
2021-06-14 13:40:01 +0200

    finished converter exeercise

---
+++ 03-react/04-degrees-converter/src/main.tsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const appDiv = document.getElementById("app");
+
+if (!(appDiv instanceof HTMLDivElement)) {
+	throw new Error("No div with id 'app' found");
+}
+
+/**
+ * Convert a string to an integer.
+ * 
+ * Returns 0 if the string can not be converted.
+ */
+const toInt = (
+	value: string
+): number => {
+	const result = parseInt(value);
+	return isNaN(result) ? 0 : result;
+};
+
+const App = (): JSX.Element => {
+	const [a, setA] = React.useState("");
+	const [b, setB] = React.useState("");
+
+  const conversionF = (e:React.ChangeEvent<HTMLInputElement>) => {
+		setB(e.target.value);
+		const celsius = toInt(e.target.value) *1.8 + 32;
+		setA(celsius.toString());
+	}
+  
+	const conversionC = (e:React.ChangeEvent<HTMLInputElement>) => {
+		setA(e.target.value);
+		const fahrenheit = toInt(e.target.value)  - 32 / 1.8;
+		setB(fahrenheit.toString());
+	}
+	return (
+		<main>
+			<input
+				type="number"
+				value={a}
+				placeholder="C°"
+				onChange={(e) => conversionC(e)}
+			/>
+			{" = "}
+			<input
+				type="number"
+				value={b}
+				placeholder="F°"
+				onChange={(e) => conversionF(e)}
+			/>
+		</main>
+	);
+};
+
+ReactDOM.render(<App />, appDiv);
